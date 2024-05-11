15ebced11b66d1468e1eebfdcd4ff2336591189e
Pei <104003911+peiiqii@users.noreply.github.com>
2024-05-11 15:33:31 +0000

    Delete Linux/Task1 directory

--- Linux/Task1/hello_world.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "hello_world.h"
-
-void hello() {
-	std::cout << "Hello World!" << std::endl;
-}
--- Linux/Task1/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "hello_world.h"
-#include <iostream>
-
-int main(){	
-	hello();
-	return 0;
-}
