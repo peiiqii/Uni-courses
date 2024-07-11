8445cad334aaa31c69d0fed7163f1f4ccbc93705
Pei <lpq1104@gmail.com>
2024-07-11 21:33:23 +0200

    Task 1.5: Flow control

--- Linux/Prak01_BashScripting/arrays.sh
+++ Linux/Prak01_BashScripting/arrays.sh
@@ -2,30 +2,8 @@
 
 MyArray=("Element 1" "Element 2" 42 1337)
 
-echo $MyArray[0]
-echo $MyArray[1]
-echo $MyArray[2]
-echo $MyArray[3]
-
-echo ${MyArray[0]}
-echo ${MyArray[1]}
-echo ${MyArray[2]}
-echo ${MyArray[3]}
-
-MyArray[3]="UpdatedContent"
-MyArray[4]="AppendedContent"
-
-echo ${MyArray[3]}
-echo ${MyArray[4]}
-
-echo ${#MyArray[@]}
-
-echo "Print array using loop:"
-for j in ${MyArray[@]}; do
-        echo $j
-done
-
-echo "Print array using loop:"
-for j in "${MyArray[@]}"; do
-        echo $j  
+for element in "${MyArray[@]}"; do
+	if [ ${#element} -ge 5 ]; then
+		echo $element
+	fi
 done
